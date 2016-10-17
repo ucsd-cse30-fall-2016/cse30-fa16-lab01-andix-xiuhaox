1c72bd26be5b8c096cc3e6e2346c7930b3e66e19
xiw022 <xiw022@ucsd.edu>
2016-10-16 22:43:27 -0700

    Update bits_ARM.s

--- bits_ARM.s
+++ bits_ARM.s
@@ -184,7 +184,8 @@ fitsBits_ARM:
 negate_ARM:
     @ Save caller's registers on the stack
     push {r4-r11, ip, lr}
-    NEG r0, r0
+    MVN r0, r0
+    ADD r0, r0, #1
     pop {r4-r11, ip, lr}
 
     @ ARM equivalent of return
